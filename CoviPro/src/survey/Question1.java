aa9a7f79f0df9ef4faf09b4565610ab7ec5bccf0
hanYijin <gksdlwls123@naver.com>
2021-05-31 16:50:03 +0900

    Update Question1.java

--- CoviPro/src/survey/Question1.java
+++ CoviPro/src/survey/Question1.java
@@ -1,46 +1,48 @@
 package survey;
 
+import java.util.List;
+
 public class Question1 {
 	private int q1_ID;
-	private String answer1;
-	private String answer2;
-	private String answer3;
-	private String answer4;
-	private String answer5;
+	private List<String> answer1;
+	private List<String> answer2;
+	private List<String> answer3;
+	private List<String> answer4;
+	private List<String> answer5;
 	public int getQ1_ID() {
 		return q1_ID;
 	}
 	public void setQ1_ID(int q1_ID) {
 		this.q1_ID = q1_ID;
 	}
-	public String getAnswer1() {
+	public List<String> getAnswer1() {
 		return answer1;
 	}
-	public void setAnswer1(String answer1) {
+	public void setAnswer1(List<String> answer1) {
 		this.answer1 = answer1;
 	}
-	public String getAnswer2() {
+	public List<String> getAnswer2() {
 		return answer2;
 	}
-	public void setAnswer2(String answer2) {
+	public void setAnswer2(List<String> answer2) {
 		this.answer2 = answer2;
 	}
-	public String getAnswer3() {
+	public  List<String> getAnswer3() {
 		return answer3;
 	}
-	public void setAnswer3(String answer3) {
+	public void setAnswer3( List<String> answer3) {
 		this.answer3 = answer3;
 	}
-	public String getAnswer4() {
+	public  List<String> getAnswer4() {
 		return answer4;
 	}
-	public void setAnswer4(String answer4) {
+	public void setAnswer4( List<String> answer4) {
 		this.answer4 = answer4;
 	}
-	public String getAnswer5() {
+	public  List<String> getAnswer5() {
 		return answer5;
 	}
-	public void setAnswer5(String answer5) {
+	public void setAnswer5( List<String> answer5) {
 		this.answer5 = answer5;
 	}
 	
